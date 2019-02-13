f1f0fdafbaf92136f95b4f1d200ed7eb3dfab779
seonghyeon <tjdgusdkf@gmail.com>
2019-02-13 17:24:26 +0900

    aa2

--- project04/settings.py
+++ project04/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '43u^me^5h2^uwkztn#s(t#271dk%@l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'afternoon-coast-96202.herokuapp.com']
 
 
 # Application definition
