76fbc4f08ccb3adaf7f6310a4196f4636759e514
seonghyeon <tjdgusdkf@gmail.com>
2019-02-26 16:36:19 +0900

    first project commit

--- project04/settings.py
+++ project04/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio', 'static')
+    os.path.join(BASE_DIR, 'portfolio', 'static'),
+    os.path.join(BASE_DIR, 'blog', 'static', 'css')
 ] #static 파일들이 현재 어디에 있는지
  
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
