29f6a9e3c1a59c110a82c1c833810683386191ea
Seonghyon Bae <tjdgusdkf@likelion.org>
2019-02-28 17:40:59 +0900

    css change

--- blog/form.py
+++ blog/form.py
@@ -4,9 +4,16 @@ from .models import Blog
 #class BlogPost(forms.Form) #모델 기반이 아닌 임의의 입력공간일 경우
 
 class BlogPost(forms.ModelForm): 
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super(BlogPost, self).__init__(*args, **kwargs)
+        
     class Meta:
         model = Blog #blog 모델의
         fields = ['title', 'body'] #title과 body를 입력받을 공간
-
+        labels = {
+            "title": "제목",
+            "body": "내용"
+        }
 
 
